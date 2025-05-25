e32db3fb3e3f638d7572752a93d72d20bb38828b
Laura <dsw56009@student.dsw.edu.pl>
2025-05-25 22:08:43 +0200

    Dodanie opcji --help z listą dostępnych flag

--- skrypt.sh
+++ skrypt.sh
@@ -18,6 +18,14 @@ if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     echo "Created by: skrypt.sh" >> "$filename"
     echo "Date: $(date "+%Y-%m-%d %H:%M:%S")" >> "$filename"
   done
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Dostępne opcje:"
+  echo "--date, -d         Wyświetla aktualną datę"
+  echo "--logs, -l [N]     Tworzy N plików logx.txt (domyślnie 100)"
+  echo "--help, -h         Wyświetla tę pomoc"
+  exit 0
+fi
 fi
 
 if [[ "$1" == "--init" ]]; then
